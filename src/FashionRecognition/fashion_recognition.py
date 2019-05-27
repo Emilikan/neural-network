259713fd73a29d001440a4f47e4c2953d63909be
Emilikan <emilqiwi@gmail.com>
2019-05-27 15:32:53 +0300

    add info in fashion recognition about nn

--- src/FashionRecognition/fashion_recognition.py
+++ src/FashionRecognition/fashion_recognition.py
@@ -11,13 +11,16 @@ import numpy as np
 # Преобразование размерности изображений (полносвязная нейронная сеть не может работать с 2D данными (28х28),
 # поэтому преобразовываем в одномерный массив (60000 изображений, 784=28*28 пикселей в каждом))
 x_train = x_train.reshape(60000, 784)
+x_test = x_test.reshape(10000, 784)
 
 # Нормализация данных
-x_train /= 255
+x_train = x_train / 255
+x_test = x_test / 255
 
 
 # Преобразуем метки в категории
 y_train = utils.to_categorical(y_train, 10)
+y_test = utils.to_categorical(y_test, 10)
 
 # Названия классов (чтобы выводить названия)
 classes = ['футболка', 'брюки', 'свитер', 'платье', 'пальто', 'туфли', 'рубашка', 'кроссовки', 'сумка', 'ботинки']
@@ -44,12 +47,18 @@ model.compile(loss="categorical_crossentropy", optimizer="SGD", metrics=["accura
 # одного и того же набора изображений, verbose - печатаем прогресс нейронной сети)
 model.fit(x_train, y_train, batch_size=200, epochs=100, verbose=1)
 
+# Оцениваем качество сети на тестовых данных
+score = model.evaluate(x_test, y_test, verbose=1)
+
+# Доля правильных ответов
+print("Доля прав ответов: ", round(score[1]*100, 4))
+
 
 # Распознаем предметы данных
-predictions = model.predict(x_train)
+predictions = model.predict(x_test)
 
 # Выводим номер класса, предсказанный нейронной сетья
 print(np.argmax(predictions[0]))
 
 # Выводим правильный номер класса
-print(np.argmax(y_train[0]))
+print(np.argmax(y_test[0]))
